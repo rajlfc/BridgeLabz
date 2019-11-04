16c0399f6ce7bd1056c80a50b087555a048d101d
rajlfc <42748727+rajlfc@users.noreply.github.com>
2019-11-04 14:36:21 +0530

    Add files via upload

---
+++ ThirdViewController.swift
@@ -0,0 +1,193 @@
+//
+//  ThirdViewController.swift
+//  FundooApp
+//
+//  Created by admin on 20/09/19.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+struct Register: Encodable {
+    let firstName: String
+    let lastName: String
+    let service:String = "advance"
+    let username : String
+    let email : String
+    let password : String
+    let emailVerified : Bool = true
+    let id : String = "none"
+}
+
+class ThirdViewController: UIViewController,UITextFieldDelegate {
+
+    @IBOutlet weak var imageview: UIImageView!
+    
+    @IBOutlet weak var namefield: UITextField!
+    @IBOutlet weak var EmailField: UITextField!
+    //@IBOutlet weak var PassField: UITextField!
+    
+    @IBOutlet weak var password: UITextField!
+    //@IBOutlet weak var Passfield: UITextField!
+    @IBOutlet weak var useridfield: UITextField!
+    @IBOutlet weak var ConfirmPassField: UITextField!
+    @IBOutlet weak var securityfield: UITextField!
+    var emailverify : Int?
+    var passverify : Int?
+    var myArrayofRiceInventory = [Inventory]()
+    var myArrayofRiceInventory2 = [String:[Inventory]]()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        UserDefaults.standard.string(forKey: )
+//        imageview.image = UIImage(named: "Image-3")?.alpha(0.4)
+//        self.view.sendSubviewToBack(imageview)
+        EmailField.setPadding()
+        EmailField.setBottomBorder()
+        password.setPadding()
+        password.setBottomBorder()
+        ConfirmPassField.setPadding()
+        ConfirmPassField.setBottomBorder()
+        securityfield.setPadding()
+        securityfield.setBottomBorder()
+        // Do any additional setup after loading the view.
+        EmailField.tag = 1001
+        password.tag = 1002
+//        KeychainWrapper.standard.set(PassField.text!, forKey: EmailField.text!)
+     }
+    @IBAction func passwordverify(_ sender: UITextField) {
+        
+        if sender.tag == 1002
+        {
+            if Validation.shared.isValidPass(passStr: password.text!)
+            {
+                passverify = 1
+                let alertController:UIAlertController = UIAlertController(title: "Message", message: "Password Strength is Strong", preferredStyle: UIAlertController.Style.alert)
+                let alertAction:UIAlertAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler:nil)
+                alertController.addAction(alertAction)
+                present(alertController, animated: true, completion: nil)
+                print(passverify)
+            }
+            else
+            {
+                passverify = 0
+                let alertController:UIAlertController = UIAlertController(title: "Message", message: "Password Strength is weak please make a strong password", preferredStyle: UIAlertController.Style.alert)
+                let alertAction:UIAlertAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler:nil)
+                alertController.addAction(alertAction)
+                present(alertController, animated: true, completion: nil)
+                print(passverify)
+            }
+        }
+    }
+    
+    @IBAction func alertPassword(_ sender: Any) {
+        
+        let alertController:UIAlertController = UIAlertController(title: "Message", message: "Password should be minimum 8 characters long and 1 uppercase 1 lowercase 1 special character and 1 numeric", preferredStyle: UIAlertController.Style.alert)
+        let alertAction:UIAlertAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler:nil)
+        alertController.addAction(alertAction)
+        present(alertController, animated: true, completion: nil)
+    }
+    
+//    @IBAction func passwordverify(_ sender: UITextField) {
+//
+//
+//
+//
+//    }
+    @IBAction func emailverify(_ sender: UITextField) {
+        
+        //alertemailstart()
+        if sender.tag == 1001
+        {
+            if Validation.shared.isValidEmail(emailStr: EmailField.text!)
+            {
+                emailverify = 1
+                let alertController:UIAlertController = UIAlertController(title: "Message", message: "Email Verified", preferredStyle: UIAlertController.Style.alert)
+                let alertAction:UIAlertAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler:nil)
+                alertController.addAction(alertAction)
+                present(alertController, animated: true, completion: nil)
+            }
+            else
+            {
+                emailverify = 0
+                let alertController:UIAlertController = UIAlertController(title: "Message", message: "Email not supported", preferredStyle: UIAlertController.Style.alert)
+                let alertAction:UIAlertAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler:nil)
+                alertController.addAction(alertAction)
+                present(alertController, animated: true, completion: nil)
+            }
+        }
+        
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBAction func onsave(_ sender: Any) {
+
+//        let filepath = "/Users/admin/BridgeLabz/Week3 JSON/FundooApp/FundooApp/login.json"
+//        let fileData = FileManager.default.contents(atPath: filepath)
+//        let encoder = JSONEncoder()
+//        let decoder = JSONDecoder()
+//        if password.text == ConfirmPassField.text  && emailverify == 1 && passverify == 1
+//        {
+//            let d1 = Inventory(emailid: EmailField.text!,security: securityfield.text!)
+//            do{
+//
+//                let jsonDictionary : [String :[Inventory]] = try decoder.decode(Dictionary.self, from: fileData!)
+//                myArrayofRiceInventory = jsonDictionary["details"]!
+//                myArrayofRiceInventory.append(d1)
+//                KeychainWrapper.standard.set(password.text!, forKey: EmailField.text!)
+//                myArrayofRiceInventory2 = ["details": myArrayofRiceInventory]
+//                let jsondata = try encoder.encode(myArrayofRiceInventory2)
+//                let url = URL(fileURLWithPath: filepath)
+//                try jsondata.write(to: url)
+//                   alertsuccessfulregister()
+//                let main = UIStoryboard(name: "Main", bundle: nil)
+//                let first = main.instantiateViewController(withIdentifier: "FirstVC")
+//                self.present(first, animated: true, completion: nil)
+//
+//
+//            }catch let error{
+//                print("Not Found")
+//            }
+//        }
+//        else if password.text != ConfirmPassField.text
+//        {
+//            alertpasswordmissmatch()
+//        }
+//        else if emailverify == 0
+//        {
+//            alertemailverify()
+//        }
+//        else if passverify == 0
+//        {
+//            alertpasswordstrength()
+//        }
+    
+          var email = EmailField.text
+          var passwordval = password.text
+          var name = namefield.text
+        let register = Register(firstName: name!, lastName: name!, username: name!, email: email!, password: passwordval!)
+        print(register)
+        AF.request("http://fundoonotes.incubation.bridgelabz.com/api/user/userSignUp",
+                   method: .post,
+                   parameters: register,
+                   encoder: JSONParameterEncoder.default).response { response in
+                    debugPrint(response)
+        }
+    }
+    
+    @IBAction func gotologin(_ sender: Any) {
+        
+        
+    }
+    
+}
+
